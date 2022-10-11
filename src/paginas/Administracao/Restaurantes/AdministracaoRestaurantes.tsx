c99a1a21cb57ccc2dc4c9dfd97e543557f2eb0e2
José Alfredo <jose.alfredo@veri.com.br>
2022-10-11 14:17:55 -0300

    Primeiro commit

---
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -0,0 +1,33 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { useState } from "react";
+import IRestaurante from "../../../interfaces/IRestaurante";
+
+const AdministracaoRestaurantes = () => {
+
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes;
